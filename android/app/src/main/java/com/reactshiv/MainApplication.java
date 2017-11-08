2de333b9d6ad9635fc8602c05f87fba9fe391065
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-07 22:49:43 -0800

    Added changes to MainApplication.java for android FB sign in integration

--- android/app/src/main/java/com/reactshiv/MainApplication.java
+++ android/app/src/main/java/com/reactshiv/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactshiv;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -10,6 +11,10 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new FBSDKPackage()
       );
     }
 
